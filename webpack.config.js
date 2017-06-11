cc4205c097d17a20bb54efd02e556d5f3a2a57f6
Tyler Waters <tyler.waters@gmail.com>
2017-06-10 22:00:30 -0700

    Cleaning up a few things

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const webpack = require('webpack');
-const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+/* eslint-env node */
+
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-	entry: path.resolve('./src/js/app.js'),
-	output: {
-		filename: '[chunkhash].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['es2015']
-				}
-			},
+  entry: path.resolve('./src/js/app.js'),
+  output: {
+    filename: '[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      },
       {
         test: /\.(less|css)$/,
         use: ExtractTextPlugin.extract({
@@ -27,10 +27,10 @@ module.exports = {
             {
               loader: 'css-loader',
               options: {
-								importLoaders: 1,
+                importLoaders: 1,
                 sourceMap: true,
-								modules: true,
-								camelCase: true
+                modules: true,
+                camelCase: true
               }
             },
             {
@@ -43,14 +43,14 @@ module.exports = {
           fallback: 'style-loader'
         })
       }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/html/index.html',
-			filename: '../index.html',
-			minify: {collapseWhitespace: true}
-		}),
-		new ExtractTextPlugin({filename: '[chunkhash].css', allChunks: true})
-	]
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.html',
+      filename: '../index.html',
+      minify: {collapseWhitespace: true}
+    }),
+    new ExtractTextPlugin({filename: '[chunkhash].css', allChunks: true})
+  ]
 };
