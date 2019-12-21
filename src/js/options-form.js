1ea70ff21ce2c6c8adc2cd74e9e2071ca5e59fee
Tyler Waters <tyler.waters@gmail.com>
2019-12-21 13:24:49 -0800

    Add dark mode option

--- src/js/options-form.js
+++ src/js/options-form.js
@@ -12,6 +12,14 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs.checked = options.tabType === 'tabs'
   tabs.addEventListener('change', e => (options.tabType = e.target.value))
 
+  const darkMode = document.getElementById('theme[dark]')
+  darkMode.checked = options.theme === 'dark'
+  darkMode.addEventListener('change', e => (options.theme = e.target.value))
+
+  const lightMode = document.getElementById('theme[light]')
+  lightMode.checked = options.theme === 'light'
+  lightMode.addEventListener('change', e => (options.theme = e.target.value))
+
   const length = document.getElementById('length')
   length.value = options.length
   length.addEventListener('change', e => (options.length = e.target.value))
--- src/js/options.js
+++ src/js/options.js
@@ -1,10 +1,25 @@
+import { darkMode, lightMode } from '../styles/app.css'
+
 const length = Symbol('length')
 const tabType = Symbol('tab-type')
+const theme = Symbol('theme')
+
+const updateTheme = theme => {
+  const bodyClass = theme === 'dark' ? darkMode : lightMode
+  document.body.classList.remove(darkMode, lightMode)
+  document.body.classList.add(bodyClass)
+}
 
 class Options {
   constructor() {
     this[length] = window.localStorage.getItem('length') || 4
     this[tabType] = window.localStorage.getItem('tab-type') || 'spaces'
+    this[theme] =
+      window.localStorage.getItem('theme') ||
+      (window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light')
+    updateTheme(this[theme])
   }
 
   get length() {
@@ -23,6 +38,16 @@ class Options {
   set tabType(value) {
     window.localStorage.setItem('tab-type', value)
     this[tabType] = value
+  }
+
+  get theme() {
+    return this[theme]
+  }
+
+  set theme(value) {
+    window.localStorage.setItem('theme', value)
+    updateTheme(value)
+    this[theme] = value
   }
 }
 
